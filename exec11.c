50c57744cb16fd9446c45448f7885cf841d33816
samtaborsky <s.taborsky1@gmail.com>
2020-10-13 17:16:17 +0200

    exercise 11

---
+++ exec11.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);
+
+    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+        c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+        printf("%c is a vowel.\n", c);
+    }
+    else if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+        printf("%c is a consonant.\n", c);
+    }
+    else {
+        printf("%c is not an alphabet.\n", c);
+    }
+
+    return 0;
+}
