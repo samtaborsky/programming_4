6036a8e754208d2d006a802648df60abdc0b0f45
Samuel Táborský <s.taborsky1@gmail.com>
2021-02-02 19:25:58 +0100

    typo fix

--- pointers/exec1.c
+++ pointers/exec1.c
@@ -11,26 +11,25 @@ int main()
     double x;
     int array1[10];
 
-    printf("k - address: %p size: %d b\n", &k, (int)sizeof(k));
-    printf("l - address: %p size: %d b\n", &l, (int)sizeof(l));
-    printf("y - address: %p size: %d b\n", &y, (int)sizeof(y));
-    printf("array2 - size: %d b\n"
-           "first el. - address: %p size: %d b\n"
-           "last el. - address: %p size: %d b\n\n",
+    printf("k - address: %p size: %d B\n", &k, (int)sizeof(k));
+    printf("l - address: %p size: %d B\n", &l, (int)sizeof(l));
+    printf("y - address: %p size: %d B\n", &y, (int)sizeof(y));
+    printf("array2 - size: %d B\n"
+           "first el. - address: %p size: %d B\n"
+           "last el. - address: %p size: %d B\n\n",
            (int)sizeof(array2), &array2[0], (int)sizeof(array2[0]), &array2[10], (int)sizeof(array2[10]));
 
-    printf("a - address: %p size: %d b\n", &a, (int)sizeof(a));
-    printf("b - address: %p size: %d b\n", &b, (int)sizeof(b));
-    printf("c - address: %p size: %d b\n", &c, (int)sizeof(c));
-    printf("x - address: %p size: %d b\n", &x, (int)sizeof(x));
+    printf("a - address: %p size: %d B\n", &a, (int)sizeof(a));
+    printf("b - address: %p size: %d B\n", &b, (int)sizeof(b));
+    printf("c - address: %p size: %d B\n", &c, (int)sizeof(c));
+    printf("x - address: %p size: %d B\n", &x, (int)sizeof(x));
     printf("array1 - size: %d b\n"
-           "first el. - address: %p size: %d b\n"
-           "last el. - address: %p size: %d b\n\n",
+           "first el. - address: %p size: %d B\n"
+           "last el. - address: %p size: %d B\n\n",
            (int)sizeof(array1), &array1[0], (int)sizeof(array1[0]), &array1[10], (int)sizeof(array1[10]));
 
     // variable a - hexadecimal value and formal pointer representation
     printf("%x   %p\n", &a, &a);
-
 
     return 0;
 }
