ddd503939b0236bdb7156d9aea95a360039e9d13
Samuel Táborský <s.taborsky1@gmail.com>
2021-03-28 23:24:10 +0200

    test algorithm in exercise 1

---
+++ file_io/exec1-test.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 1000
+
+long count_chars(FILE *input) {
+    fseek(input, 0, SEEK_END);
+    return ftell(input);
+}
+
+int main() {
+    long count;
+    char character;
+    FILE *input, *output;
+
+    input = fopen("input_text.txt", "r");
+    output = fopen("output_text.txt", "w");
+
+    if (input == NULL) {
+        printf("Input file could not be opened, exiting...\n");
+        return 1;
+    }
+    if (output == NULL) {
+        printf("Output file could not be opened, exiting...\n");
+        return 1;
+    }
+
+    count = count_chars(input);
+    fseek(input, -1L, SEEK_END);
+
+    while (count > 0) {
+        character = fgetc(input);
+        if (character == 10) {}
+        if (character == 13) {}
+        else {fputc(character, output);}
+        fseek(input, -2L, SEEK_CUR);
+        count--;
+    }
+
+    fclose(input);
+    fclose(output);
+    return 0;
+}
