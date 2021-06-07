498d8135a72f6a62070b49132e1f34c4440c47f5
Samuel Táborský <s.taborsky1@gmail.com>
2021-06-07 09:02:08 +0200

    program 01

---
+++ algorithms/exec1.c
@@ -0,0 +1,37 @@
+/*
+ * Author: Samuel Táborský
+ * Last revision: 6.7.2021
+ *
+ * Program 1
+ *
+ * This program:
+ * - reads 2 integers from STDIN
+ * - calculates and displays their GCD
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 1000
+
+int main(void) {
+    int number1, number2;
+    // input
+    printf("Enter 2 numbers: ");
+    scanf("%d %d", &number1, &number2);
+
+    // gcd calculation
+    int a = number1, b = number2, c;
+    while (b > 0) {
+        c = a % b;
+        a = b;
+        b = c;
+    }
+
+    // output
+    printf("%d\n", a);
+
+    return 0;
+}
